abcba5b20ec7d208f31ccfe30c595d97223752c2
moomoocalf <moomoocalf@df0a4146-c87e-97af-0b4c-28a967717587>
2011-11-17 02:13:23 +0000

    Project up to date as of November 14, 2011

--- trunk/src/GameState.java
+++ trunk/src/GameState.java
@@ -52,14 +52,33 @@ public class GameState
 		menu = gameloop.makeButton("Menu", gameloop.getImage("Art/Menu.png", 255, 255, 255), 169, 51);
 		instructions = new Sprite(gameloop.getImage("Art/Instructions1.png", 255, 255, 255));
 		emptyTileSprite = new Sprite(gameloop.getImage("Art/Grass.png"));
-		arrowPanelSprite = new Sprite(gameloop.getImage("Art/ArrowPanel.png"), 32, 32);
-		arrowPanelSprite.addFrame(gameloop.getImage("Art/ArrowPanel.png"), 32, 0);
+		arrowPanelSprite = new Sprite(32, 32);
+		arrowPanelSprite.addFrames(gameloop.getImage("Art/ArrowPanel.png"), 0,0, 32,0);
 		arrowPanelSprite.framerate(1);
 		goalSprite = new Sprite(gameloop.getImage("Art/Goal.png"));
 		pitSprite = new Sprite(gameloop.getImage("Art/Pit.png"));
 		
-		girlSprite = new Sprite(gameloop.getImage("Art/Girl.png"));
-		boundarySprite = new Sprite(gameloop.getImage("Art/Boundary.png", 255, 255, 255));
+		girlSprite = new Sprite(30, 35);
+		girlSprite.addFrames(gameloop.getImage("Art/belarus2small.png"), 0,0, 30,0, 60,0, 90,0,
+																			0,35, 30,35, 60,35, 90,35,
+																			0,70, 30,70, 60,70, 90,70,
+																			0,105, 30,105, 60,105, 90,105);
+		
+		girlSprite.defineSequence("spin", 0, 4, 12, 8);
+		
+		girlSprite.defineSequence("upStand", 12);
+		girlSprite.defineSequence("downStand", 0);
+		girlSprite.defineSequence("leftStand", 4);
+		girlSprite.defineSequence("rightStand", 8);
+		
+		girlSprite.defineSequence("upMove", 12, 13, 14, 15);
+		girlSprite.defineSequence("downMove", 0, 1, 2, 3);
+		girlSprite.defineSequence("leftMove", 4, 5, 6, 7);
+		girlSprite.defineSequence("rightMove", 8, 9, 10, 11);
+		
+		girlSprite.framerate(7);
+		//girlSprite = new Sprite(gameloop.getImage("Art/Girl.png"));
+		boundarySprite = new Sprite(gameloop.getImage("Art/Boundary.png"));
 		doorSprite = new Sprite(gameloop.getImage("Art/Door.png"));
 		helperCharacterSprite = new Sprite(gameloop.getImage("Art/HelperCharacter.png"));
 		keySprite = new Sprite(gameloop.getImage("Art/Key.png"));
@@ -134,35 +153,71 @@ public class GameState
 	{
 		grid = new Grid();
 		
-		grid.setTileDown(new ArrowPanel(5, 3, Direction.Down));
-		grid.setTileDown(new ArrowPanel(5, 20, Direction.Right));
-		grid.setTileDown(new ArrowPanel(6, 20, Direction.Right));
-		grid.setTileDown(new ArrowPanel(20, 20, Direction.Up));
-		grid.setTileDown(new ArrowPanel(20, 3, Direction.Left));
-		grid.setTileDown(new Goal(12, 12));
-		grid.setTileDown(new Pit(0, 7));
-		grid.setTileDown(new Pit(8, 9));
-//		grid.setTileDown(new Pit(6, 20));
+		grid.setTileDown(new ArrowPanel(20, 5, Direction.Down));
+		grid.setTileDown(new Goal(0, 0));
+		grid.setTileDown(new Pit(0, 23));
 		
-		grid.setPieceDown(new Boundary(2, 2));
-		grid.setPieceDown(new Door(5, 11));
-		grid.setPieceDown(new HelperCharacter(3, 2));
-		grid.setPieceDown(new Key(4, 4));
-		grid.setPieceDown(new PushableBlock(8, 2));
-		grid.setPieceDown(new PushableBlock(11, 2));
-		grid.setPieceDown(new Spikeball(6, 2, Direction.Down));
-		grid.setPieceDown(new SpikeTrap(8, 8));
-//		grid.setPieceDown(new Spikeball(1, 11, Direction.Right));
+		grid.setPieceDown(new Spikeball(5, 5, Direction.Left));
+		grid.setPieceDown(new SpikeTrap(5, 9));
 		
-		Girl girl = new Girl(0,0);
+		grid.setPieceDown(new HelperCharacter(2, 8));
+		grid.setPieceDown(new HelperCharacter(27, 6));
+		grid.setPieceDown(new HelperCharacter(18, 4));
+		
+		grid.setPieceDown(new Key(2, 0));
+		grid.setPieceDown(new Key(2, 15));
+		grid.setPieceDown(new Key(4, 9));
+		grid.setPieceDown(new Key(6, 9));
+		grid.setPieceDown(new Key(4, 7));
+		grid.setPieceDown(new Key(14, 9));
+		grid.setPieceDown(new Key(18, 5));
+		grid.setPieceDown(new Key(25, 20));
+		grid.setPieceDown(new Key(7, 1));
+		
+		grid.setPieceDown(new Door(5, 10));
+		grid.setPieceDown(new Door(3, 17));
+		grid.setPieceDown(new Door(6, 17));
+		grid.setPieceDown(new Door(17, 19));
+		grid.setPieceDown(new Door(5, 8));
+		grid.setPieceDown(new Door(9, 14));
+		grid.setPieceDown(new Door(14, 8));
+		grid.setPieceDown(new Door(7, 11));
+		grid.setPieceDown(new Door(11, 15));
+		grid.setPieceDown(new Door(25, 13));
+		grid.setPieceDown(new Door(0, 1));
+		grid.setPieceDown(new Door(1, 0));
+		
+//		grid.setTileDown(new ArrowPanel(5, 3, Direction.Down));
+//		grid.setTileDown(new ArrowPanel(5, 20, Direction.Right));
+//		grid.setTileDown(new ArrowPanel(6, 20, Direction.Right));
+//		grid.setTileDown(new ArrowPanel(20, 20, Direction.Up));
+//		grid.setTileDown(new ArrowPanel(20, 3, Direction.Left));
+//		grid.setTileDown(new Goal(12, 12));
+//		grid.setTileDown(new Pit(0, 7));
+//		grid.setTileDown(new Pit(8, 9));
+////		grid.setTileDown(new Pit(6, 20));
+//		
+//		grid.setPieceDown(new Boundary(2, 2));
+//		grid.setPieceDown(new Door(5, 11));
+//		grid.setPieceDown(new HelperCharacter(3, 2));
+//		grid.setPieceDown(new Key(4, 4));
+//		grid.setPieceDown(new PushableBlock(8, 2));
+//		grid.setPieceDown(new PushableBlock(11, 2));
+//		grid.setPieceDown(new Spikeball(6, 2, Direction.Down));
+//		grid.setPieceDown(new SpikeTrap(8, 8));
+////		grid.setPieceDown(new Spikeball(1, 11, Direction.Right));
+//		
+//		Girl girl = new Girl(0,0);
+		
+		Girl girl = new Girl(20, 20);
 		grid.setPieceDown(girl);
 		
 		player = new Player(girl);
 		
-		SoundPlayer.playBGM(SoundPlayer.grandWaltz);
+		SoundPlayer.playBGM(SoundPlayer.hopefulUnderstanding);
 		
 //		try {
-//			writeLevel("Levels/SpikeTrapTest1");
+//			writeLevel("Levels/LotsOfKeys");
 //		} catch (IOException e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
@@ -176,7 +231,52 @@ public class GameState
 	// Moves the piece that the player currently controls.
 	public void move(Direction d)
 	{
+		boolean wasMoving = player.getCurrentPiece().isMoving();
+		Direction prevDirection = player.getCurrentPiece().getLastDirectionMoved();
+		
 		grid.movePiece(player.getX(), player.getY(), d);
+		
+		if (player.getCurrentPiece() instanceof Girl)
+		{
+			Girl girl = (Girl)player.getCurrentPiece();
+			
+			if (girl.getLastDirectionMoved() == Direction.Up)
+			{
+				girlSprite.play("upMove");
+				
+				if (!wasMoving || prevDirection != Direction.Up)
+				{
+					girlSprite.setToFrame(12);
+				}
+			}
+			else if (girl.getLastDirectionMoved() == Direction.Down)
+			{
+				girlSprite.play("downMove");
+				
+				if (!wasMoving || prevDirection != Direction.Down)
+				{
+					girlSprite.setToFrame(0);
+				}
+			}
+			else if (girl.getLastDirectionMoved() == Direction.Left)
+			{	
+				girlSprite.play("leftMove");
+				
+				if (!wasMoving || prevDirection != Direction.Left)
+				{
+					girlSprite.setToFrame(4);
+				}
+			}
+			else if (girl.getLastDirectionMoved() == Direction.Right)
+			{	
+				girlSprite.play("rightMove");
+				
+				if (!wasMoving || prevDirection != Direction.Right)
+				{
+					girlSprite.setToFrame(8);
+				}
+			}
+		}
 	}
 	
 	// This function is called when the player clicks the
@@ -205,6 +305,41 @@ public class GameState
 	// lost the level.
 	public void update()
 	{
+		Girl girl = player.getGirl();
+		
+		if (girl.isBeingLaunched())
+		{
+			girlSprite.play("spin");
+		}
+		else if (girl.getLastDirectionMoved() == Direction.Up)
+		{
+			//girlSprite.setToFrame(12);
+			
+			if (!girl.isMoving())
+				girlSprite.play("upStand");
+		}
+		else if (girl.getLastDirectionMoved() == Direction.Down)
+		{
+			//girlSprite.setToFrame(0);
+			
+			if (!girl.isMoving())
+				girlSprite.play("downStand");
+		}
+		else if (girl.getLastDirectionMoved() == Direction.Left)
+		{
+			//girlSprite.setToFrame(4);
+			
+			if (!girl.isMoving())
+				girlSprite.play("leftStand");
+		}
+		else if (girl.getLastDirectionMoved() == Direction.Right)
+		{
+			girlSprite.setToFrame(8);
+			
+			if (!girl.isMoving())
+				girlSprite.play("rightStand");
+		}
+		
 		grid.update();
 		player.update();
 		
@@ -323,7 +458,7 @@ public class GameState
 					}
 				}
 			}
-		
+			
 			// draw pieces
 			for (int x = 0; x < Grid.WIDTH; x++)
 			{
@@ -336,7 +471,7 @@ public class GameState
 						continue;
 					}
 					else if (piece instanceof Girl)
-					{
+					{	
 						girlSprite.position(piece.getPixelX(), piece.getPixelY());
 						girlSprite.draw();
 					}
@@ -372,7 +507,7 @@ public class GameState
 					}
 				}
 			}
-
+			
 			menu.position(750, 25);
 			menu.show();
 			menu.draw();

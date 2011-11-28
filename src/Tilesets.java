28bd1582ebb39417f3a97ae29d6228fccc343911
moomoocalf <moomoocalf@df0a4146-c87e-97af-0b4c-28a967717587>
2011-11-28 06:43:43 +0000

    Added tileset.png to the Art folder, set up Tilesets.loadTileset(int tileset) with an if/else chain that can be filled in with code for loading the appropriate tileset.

--- src/Tilesets.java
+++ src/Tilesets.java
@@ -3,6 +3,16 @@ import ucigame.*;
 
 public class Tilesets 
 {
+	// Tileset Codes
+	public static final int busTileset = 0;
+	public static final int gardenTileset = 1;
+	public static final int houseTileset = 2;
+	public static final int sunroomTileset = 3;
+	public static final int libraryTileset = 4;
+	public static final int twistedHallTileset = 5;
+	public static final int basementTileset = 6;
+	public static final int gluttonTileset = 7;
+	
 	// Sprite list
 	public static Sprite emptyTileSprite;
 	public static Sprite arrowPanelSprite;
@@ -21,16 +31,19 @@ public class Tilesets
 	public static Sprite nullPieceSprite;
 	public static Sprite rotateButtonSprite;
 	
+	// Gameloop
+	// used for loading images
+	private static Forsaken gameloop;
+	
 	public static void initialize(Forsaken gameloop)
 	{
-		// initialize Sprites
-		emptyTileSprite = new Sprite(gameloop.getImage("Art/Grass.png"));
-		arrowPanelSprite = new Sprite(32, 32);
-		arrowPanelSprite.addFrames(gameloop.getImage("Art/ArrowPanel.png"), 0,0, 32,0);
-		arrowPanelSprite.framerate(1);
-		goalSprite = new Sprite(gameloop.getImage("Art/Goal.png"));
-		pitSprite = new Sprite(gameloop.getImage("Art/Pit.png"));
+		Tilesets.gameloop = gameloop;
 		
+		// should be initialized with busTileset once the
+		// tilesets are implemented
+		Tilesets.loadTileset(-1);
+		
+		// initialize girl sprite
 		girlSprite = new Sprite(30, 35);
 		girlSprite.addFrames(gameloop.getImage("Art/belarus2small.png"), 0,0, 30,0, 60,0, 90,0,
 																			0,35, 30,35, 60,35, 90,35,
@@ -52,20 +65,62 @@ public class Tilesets
 		girlSprite.framerate(7);
 		girlSprite.play("downStand");
 		
-		boundarySprite = new Sprite(gameloop.getImage("Art/Boundary.png"));
-		doorSprite = new Sprite(gameloop.getImage("Art/Door.png"));
-		helperCharacterSprite = new Sprite(gameloop.getImage("Art/HelperCharacter.png"));
-		keySprite = new Sprite(gameloop.getImage("Art/Key.png"));
-		pushableBlockSprite = new Sprite(gameloop.getImage("Art/PushableBlock.png"));
-		spikeballSprite = new Sprite(gameloop.getImage("Art/SpikeBall.png"));
-		spikeTrapSprite = new Sprite(gameloop.getImage("Art/SpikeTrap.png"));
-		
+		// initialize buttons for level editor
 		nullPieceSprite = new Sprite(gameloop.getImage("Art/NullPiece.png"));
 		rotateButtonSprite = new Sprite(gameloop.getImage("Art/RotateButton.png"));
 	}
 	
-	public static void loadTileSet(int tileSet)
+	public static void loadTileset(int tileset)
 	{
-		
+		if (tileset == busTileset)
+		{
+			
+		}
+		else if (tileset == gardenTileset)
+		{
+			
+		}
+		else if (tileset == houseTileset)
+		{
+			
+		}
+		else if (tileset == sunroomTileset)
+		{
+			
+		}
+		else if (tileset == libraryTileset)
+		{
+			
+		}
+		else if (tileset == twistedHallTileset)
+		{
+			
+		}
+		else if (tileset == basementTileset)
+		{
+			
+		}
+		else if (tileset == gluttonTileset)
+		{
+			
+		}
+		else // default case, should be deleted once other tilesets are implemented
+		{
+			// initialize Sprites
+			emptyTileSprite = new Sprite(gameloop.getImage("Art/Grass.png"));
+			arrowPanelSprite = new Sprite(32, 32);
+			arrowPanelSprite.addFrames(gameloop.getImage("Art/ArrowPanel.png"), /*0,0,*/ 32,0);
+			arrowPanelSprite.framerate(1);
+			goalSprite = new Sprite(gameloop.getImage("Art/Goal.png"));
+			pitSprite = new Sprite(gameloop.getImage("Art/Pit.png"));
+			
+			boundarySprite = new Sprite(gameloop.getImage("Art/Boundary.png"));
+			doorSprite = new Sprite(gameloop.getImage("Art/Door.png"));
+			helperCharacterSprite = new Sprite(gameloop.getImage("Art/HelperCharacter.png"));
+			keySprite = new Sprite(gameloop.getImage("Art/Key.png"));
+			pushableBlockSprite = new Sprite(gameloop.getImage("Art/PushableBlock.png"));
+			spikeballSprite = new Sprite(gameloop.getImage("Art/SpikeBall.png"));
+			spikeTrapSprite = new Sprite(gameloop.getImage("Art/SpikeTrap.png"));
+		}
 	}
 }
